607ae92933330a965ac934a2d49e69b6fcbbc02f
neha0890 <nehapatel30325@gmail.com>
2025-03-27 13:24:41 +0530

    import issue solve

--- Backend/utils/cronJobs.js
+++ Backend/utils/cronJobs.js
@@ -1,5 +1,5 @@
 import cron from "node-cron";
-import { deleteExpiredCoupons } from "../controllers/coupon.controller"; // Import function
+import { deleteExpiredCoupons } from "../controllers/coupon.controller.js";
 
 // Schedule the cron job to run every day at midnight
 cron.schedule("0 0 * * *", deleteExpiredCoupons);
